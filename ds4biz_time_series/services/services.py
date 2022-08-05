299c9ffc7dd9589ae971d4e536f5d6c50c775fee
Roberta <r.parisi@ilivetech.it>
2022-08-05 16:58:07 +0200

    aggiunti servizi predictor

--- ds4biz_time_series/services/services.py
+++ ds4biz_time_series/services/services.py
@@ -1,5 +1,6 @@
 import re
 import shutil
+import time
 import traceback
 
 from sanic import Sanic, Blueprint
@@ -13,12 +14,14 @@ from pathlib import Path
 # swagger_blueprint.url_prefix = "/api"
 # app.blueprint(swagger_blueprint)
 from ds4biz_time_series.config.AppConfig import REPO_PATH
+from ds4biz_time_series.dao.fs_dao import JSONFSDAO
 from ds4biz_time_series.utils.logger_utils import logger
 from ds4biz_time_series.utils.ppom_utils import get_pom_major_minor
 from sanic.exceptions import SanicException, NotFound
 
 import sanic
 
+from ds4biz_time_series.utils.service_utils import check_predictor_existence
 from ds4biz_time_series.utils.serialization_utils import serialize, deserialize
 from ds4biz_time_series.utils.service_utils import get_all
 
@@ -135,14 +138,11 @@ async def list_models(request):
 @doc.description('''
     Examples
     --------
-    obj = {"__klass__": "skt.ARIMA",
-          }
+    obj = {"__klass__": "skt.ARIMA"}
     obj = {"__klass__": "skt.NaiveForecaster",
             "strategy": "mean"
             "window_lenght"=12
-            "sp"=3
-      }
-
+            "sp"=3}
            ''')
 @doc.consumes(doc.JsonBody({}), location="body")
 @doc.consumes(doc.String(name="name"), location="path", required=True)
@@ -186,6 +186,162 @@ async def delete_model(request, name):
     return sanic.json(f'Model "{name}" deleted')
 
 
+
+
+### PREDICTORS ###
+
+@bp.get("/predictors")
+@doc.tag('predictors')
+@doc.summary("List objects in 'predictors'")
+@doc.consumes(doc.Boolean(name='info'))
+async def list_predictors(request):
+    predictors = get_all('predictors')
+    if not request.args.get('info', False):
+        return sanic.json(predictors)
+    res = []
+    for name in predictors:
+        try:
+            res_tmp = dict(name=name)
+            predictor_path = repo_path / 'predictors' / name
+            infos = deserialize(predictor_path)
+            dao = JSONFSDAO(predictor_path / 'history')
+            infos['history'] = len(dao.all())
+            #### TODO: aggiungere vere info
+            # res_tmp.update(predictor_details(infos, name, 'development', fitting.all(status='alive'), repo_path))
+            res.append(res_tmp)
+        except:
+            logger.debug('TracebackERROR: \n' + traceback.format_exc() + '\n\n')
+
+
+    return sanic.json(res)
+
+
+
+@bp.post("/predictors/<name>")
+@doc.tag('predictors')
+@doc.summary("Save an object in 'predictors'")
+@doc.description('''
+<b>model_id:</b> choose one model (see <b>POST /models/{name}</b>) or <i>"auto"</i>
+<font color="#800000"><b>AUTOML parameters must be set in the previous service!</b></font>
+<b>transformer_id:</b> choose one transformer (see <b>POST /transformers/{name}</b>) or <i>"auto"</i> or <i>"none"</i> ''')
+@doc.consumes(doc.JsonBody({}), location="body")
+@doc.consumes(doc.String(name="transformer_id"), location="query")
+@doc.consumes(doc.String(name="model_id"), location="query")
+@doc.consumes(doc.String(name="description"), location="query")
+@doc.consumes(doc.String(name="name"), location="path", required=True)
+async def create_predictor(request, name):
+    name = unquote(name)
+
+    if not re.search(r'(?i)^[a-z0-9]([a-z0-9_]*[a-z0-9])?$', name):
+        raise SanicException('No special characters (except _ in the middle of name) and whitespaces allowed',
+                             status_code=400)
+
+    predictor_path = repo_path / 'predictors' / name
+
+    check_predictor_existence(predictor_path)
+    ### transformer ###
+    transformer_id = request.args.get('transformer_id', 'auto')
+    model_id = request.args.get('model_id', 'auto')
+    blueprint = request.json
+
+    if transformer_id == 'auto':
+        # transformer = 'auto'
+        raise NotImplementedError
+    elif transformer_id == 'none':
+        # transformer = {"__klass__": "ds4biz.ct", "transformers": {}, "remainder": "passthrough"}
+        raise NotImplementedError
+    else:
+        tpath = repo_path / 'transformers' / transformer_id
+        transformer = deserialize(tpath)
+    ### model ###
+    if model_id == 'auto':
+        # mod = 'auto'
+        raise NotImplementedError
+    else:
+        mpath = repo_path / 'models' / model_id
+        mod = deserialize(mpath)
+    ### blueprint ###
+    if blueprint:
+        if blueprint.get('transformer'):
+            transf = blueprint['transformer']
+            # check_blueprint(transf, step='transformer')
+        if blueprint.get('model'):
+            mod = blueprint['model']
+            # check_blueprint(mod, step='model')
+
+    predictor_path.mkdir(exist_ok=True, parents=True)
+    bp = dict(id=name,
+              description=request.args.get('description', ''),
+              created_on=time.time() * 1000,
+              # img=request.args.get('img', 'predictor_base'),
+              steps=dict(transformer=transformer, model=mod))
+    serialize(predictor_path, bp)
+    return sanic.json(f"Predictor '{name}' saved")
+
+
+
+
+@bp.get("/predictors/<name>")
+@doc.tag('predictors')
+@doc.summary("Display object info from 'predictors'")
+@doc.description("""
+<b>name:</b> predictor id (see <b>GET /predictors</b>)
+<b>details:</b> set to True to get more info
+<b>branch:</b> development or master (see /predictors/{name}/release</b>)""")
+# @doc.consumes(doc.String(name="branch", choices=['development', 'master']), location="query")
+@doc.consumes(doc.Boolean(name="details"), location="query")
+@doc.consumes(doc.String(name="name"), location="path", required=True)
+async def predictors_info(request, name):
+    name = unquote(name)
+
+    path = repo_path / 'predictors' / name
+    if not path.exists():
+        raise SanicException(f'Predictor "{name}" does not exist!', status_code=400)
+    infos = deserialize(path)
+    details = request.args.get('details', 'false').capitalize
+    if not details:
+        return sanic.json(infos['steps'])
+    ###########todo: SVILUPPARE PARTE SOTTO#
+
+    # branch = request.args.get('branch', 'development')
+    # infos = predictor_details(infos, name, branch, fitting.jobs, repo_path)
+
+    return sanic.json(infos)
+
+
+@bp.delete("/predictors/<name>")
+@doc.tag('predictors')
+@doc.summary("Delete an object from 'predictor'")
+@doc.consumes(doc.String(name="name"), location="path", required=True)
+async def delete_predictor(request, name):
+    name = unquote(name)
+
+    path = repo_path / 'predictors' / name
+    if not path.exists():
+        raise SanicException(f'Predictor "{name}" does not exist!', status_code=400)
+    # ##TODO: sviluppare questa parte
+    # if name in fitting.all('alive'):
+    #     dc = fitting.get_by_id(name)['dc']
+    #     cd.kill(dc.name)
+    #     msg = 'Killed'
+    #     fitting.add(name, msg)
+    #     send_message(name, msg)
+    #     fitting.remove(name)
+    #     logger.debug(f'Fitting {name} Killed')
+    # else:
+    #     cname = list(filter(lambda x: x.endswith('_'+name), cd.containers.keys()))
+    #     if cname:
+    #         cd.kill(cname[0])
+    #         logger.debug(f'Container {cname[0]} Killed')
+
+    shutil.rmtree(path)
+
+    # testset_dao = get_dataset_dao(repo_path=repo_path)
+    # testset_dao.set_coll(name)
+    # testset_dao.dropcoll()
+    # testset_dao.close()
+
+    return sanic.json(f"Predictor '{name}' deleted")
 
 @app.post("/fit")
 @doc.summary('Fit an existing predictor')
--- ds4biz_time_series/utils/service_utils.py
+++ ds4biz_time_series/utils/service_utils.py
@@ -1,6 +1,10 @@
 from pathlib import Path
 from typing import List
 
+from sanic.exceptions import SanicException
+
+
+
 from ds4biz_time_series.config.AppConfig import REPO_PATH
 from ds4biz_time_series.dao.fs_dao import FileSystemDAO
 
@@ -10,3 +14,8 @@ def get_all(repo: str) -> List[str]:
     path = repo_path / repo
     fsdao = FileSystemDAO(path)
     return sorted(fsdao.all(files=False))
+
+
+def check_predictor_existence(path:Path):
+    if path.exists():
+        raise SanicException(f"Predictor '{path.name}' already exists!", status_code=409)
