1c37822d6031e2fca5563883700d2cf754336401
Roberta Parisi <r.parisi@ilivetech.it>
2022-10-19 01:45:23 +0200

    deleted dataset component

--- ds4biz_time_series/business/custom_components/main_component.py
+++ ds4biz_time_series/business/custom_components/main_component.py
@@ -4,5 +4,6 @@ from loko_extensions.model.components import save_extensions
 from ds4biz_time_series.business.custom_components.ts_manager_components import ts_manager_component
 
 if __name__ == '__main__':
-    save_extensions([time_series_component, df_component, ts_manager_component], path="../extensions")
+    #df_component
+    save_extensions([time_series_component, ts_manager_component], path="../extensions")
     #     "url": "http://localhost:9999/routes/ds4biz-time-series/transformers"
